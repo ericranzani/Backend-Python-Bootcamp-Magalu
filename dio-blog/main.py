626143a5b962a70be702588c25b4dd85234c749a
Eric Ranzani <eric_ranzani@yahoo.com.br>
2025-11-06 12:02:44 -0300

    query parameters

--- dio-blog/main.py
+++ dio-blog/main.py
@@ -3,8 +3,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+fake_db = [
+    {'title': f'Criando uma aplicação com Django', 'date': datetime.now(timezone.utc), "published": True}, 
+    {'title': f'Internacionalizando uma app FastAPI', 'date': datetime.now(timezone.utc), "published": True},
+    {'title': f'Criando uma aplicação com Flask', 'date': datetime.now(timezone.utc), "published": True}, 
+    {'title': f'Internacionalizando uma app Starlett', 'date': datetime.now(timezone.utc), "published": False},
+]
+
+# query parameters
+@app.get('/posts')
+def read_posts( published: bool, skip: int = 0, limit: int = len(fake_db)):
+    # return fake_db[skip: skip + limit] # simpler version - precisa tirar o valor boolean
+    return [post for post in fake_db[skip: skip + limit] if post['published'] is published]
+
+
+# path parameters
 @app.get('/posts/{framework}')
-def read_root(framework: str):
+def read_framework_posts(framework: str):
     return {
         "posts": [
             {'title': f'Criando uma aplicação com {framework}', 'date': datetime.now(timezone.utc)}, 
