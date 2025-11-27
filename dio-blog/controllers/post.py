47cad56135bdb067ae217735307af394db73b283
Eric Ranzani <eric_ranzani@yahoo.com.br>
2025-11-27 15:17:17 -0300

    refatoração do codigo mais o termino das ações do crud

--- dio-blog/controllers/post.py
+++ dio-blog/controllers/post.py
@@ -1,21 +1,33 @@
-from fastapi import Response, status, APIRouter
-from schemas.post import PostIn
+from fastapi import status, APIRouter
+from schemas.post import PostIn, PostUpdateIn
 from views.post import PostOut
+from services.post import PostService
+from models.post import posts
+from database import database
 
 router = APIRouter(prefix='/posts')
 
-# request body
-@router.post('/', status_code=status.HTTP_201_CREATED, response_model=PostOut)
-def create_post(post: PostIn):
-    # fake_db.append(post.model_dump())
-    return post
+services = PostService()
 
 # query parameters
-@router.get('/', response_model=list[PostOut])
-def read_posts(
-    response: Response, 
-    published: bool, 
-    limit: int, 
-    skip: int = 0
-):
-    return []
+@router.get("/", response_model=list[PostOut])
+async def read_posts(published: bool, limit: int, skip: int = 0):
+    return await services.read_all(published=published, limit=limit, skip=skip)
+    
+# request body
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=PostOut)
+async def create_post(post: PostIn):
+    return {**post.model_dump(), "id": await services.create(post)}
+
+# buscando por id
+@router.get("/{id}", response_model=PostOut)
+async def read_post(id: int):
+    return await services.read(id)
+
+@router.patch("/{id}", response_model=PostOut)
+async def update_post(id: int, post: PostUpdateIn):
+    return await services.update(id = id, post = post)
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT, response_model=None)
+async def delete_post(id: int):
+    await services.delete(id)
--- dio-blog/main.py
+++ dio-blog/main.py
@@ -1,16 +1,7 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from controllers import post
-import sqlalchemy as sa
-import databases
-
-DATABASE_URL = "sqlite:///./blog.db"
-
-database = databases.Database(DATABASE_URL)
-metadata = sa.MetaData()
-
-# checagem para evitar problemas com o FastAPI
-engine = sa.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+from database import database, metadata, engine
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
