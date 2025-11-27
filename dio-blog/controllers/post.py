90e68b1b8619663652076b3d4386c52b3500b5e2
Eric Ranzani <eric_ranzani@yahoo.com.br>
2025-11-27 13:42:52 -0300

    instalado o sqlite e desenvolvido a conexão com o db junto com a classe posts com a tabela

--- dio-blog/controllers/post.py
+++ dio-blog/controllers/post.py
@@ -6,17 +6,10 @@ from views.post import PostOut
 
 router = APIRouter(prefix='/posts')
 
-fake_db = [
-    {'title': f'Criando uma aplicação com Django', 'date': datetime.now(timezone.utc), "published": True}, 
-    {'title': f'Internacionalizando uma app FastAPI', 'date': datetime.now(timezone.utc), "published": True},
-    {'title': f'Criando uma aplicação com Flask', 'date': datetime.now(timezone.utc), "published": True}, 
-    {'title': f'Internacionalizando uma app Starlett', 'date': datetime.now(timezone.utc), "published": False},
-]
-
 # request body
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=PostOut)
 def create_post(post: PostIn):
-    fake_db.append(post.model_dump())
+    # fake_db.append(post.model_dump())
     return post
 
 # query parameters
@@ -25,16 +18,9 @@ def read_posts(
     response: Response, 
     published: bool, 
     limit: int, 
-    skip: int = 0, 
-    ads_is: Annotated[str | None, Cookie()] = None,
-    user_agent: Annotated[str | None, Header()] = None, 
+    skip: int = 0
 ):
-    response.set_cookie(key='user', value='test_cookie')
-    print(f'Cookie: {ads_is}')
-    print(f'User-Agent: {user_agent}')
-    # return fake_db[skip: skip + limit] # simpler version - precisa tirar o valor boolean
     tail = skip + limit
-    return [post for post in fake_db[skip: tail] if post['published'] is published]
 
 # path parameters
 @router.get('/{framework}', response_model=PostOut)
--- dio-blog/main.py
+++ dio-blog/main.py
@@ -1,5 +1,23 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from controllers import post
+import sqlalchemy as sa
+import databases
 
-app = FastAPI()
+DATABASE_URL = "sqlite:///./blog.db"
+
+database = databases.Database(DATABASE_URL)
+metadata = sa.MetaData()
+
+# checagem para evitar problemas com o FastAPI
+engine = sa.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+metadata.create_all(engine)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await database.connect()
+    yield
+    await database.disconnect()
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(post.router)
