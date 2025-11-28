b11fa9b59f91ac74bf24fbdee7f8b949f18d93b3
Eric Ranzani <eric_ranzani@yahoo.com.br>
2025-11-28 13:44:34 -0300

    adicionado a rota de criar o token

--- dio-blog/controllers/post.py
+++ dio-blog/controllers/post.py
@@ -1,33 +1,30 @@
-from fastapi import status, APIRouter
+from fastapi import APIRouter, Depends, status
+
 from schemas.post import PostIn, PostUpdateIn
-from views.post import PostOut
+from security import login_required
 from services.post import PostService
-from models.post import posts
-from database import database
+from views.post import PostOut
 
-router = APIRouter(prefix='/posts')
+router = APIRouter(prefix="/posts", dependencies=[Depends(login_required)])
 
-services = PostService()
-
-# query parameters
+service = PostService()
 @router.get("/", response_model=list[PostOut])
 async def read_posts(published: bool, limit: int, skip: int = 0):
-    return await services.read_all(published=published, limit=limit, skip=skip)
-    
-# request body
+    return await service.read_all(published=published, limit=limit, skip=skip)
+
+
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=PostOut)
 async def create_post(post: PostIn):
-    return {**post.model_dump(), "id": await services.create(post)}
+    return {**post.model_dump(), "id": await service.create(post)}
 
-# buscando por id
 @router.get("/{id}", response_model=PostOut)
 async def read_post(id: int):
-    return await services.read(id)
+    return await service.read(id)
 
 @router.patch("/{id}", response_model=PostOut)
 async def update_post(id: int, post: PostUpdateIn):
-    return await services.update(id = id, post = post)
+    return await service.update(id=id, post=post)
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT, response_model=None)
 async def delete_post(id: int):
-    await services.delete(id)
+    await service.delete(id)
--- dio-blog/services/post.py
+++ dio-blog/services/post.py
@@ -1,36 +1,39 @@
-from database import database
 from databases.interfaces import Record
-from fastapi import status, HTTPException
-from schemas.post import PostIn, PostUpdateIn
+
+from database import database
+from exceptions import NotFoundPostError
 from models.post import posts
+from schemas.post import PostIn, PostUpdateIn
+
 
 class PostService:
     async def read_all(self, published: bool, limit: int, skip: int = 0) -> list[Record]:
-        query = posts.select().limit(limit).offset(skip)   
+        query = posts.select().where(posts.c.published == published).limit(limit).offset(skip)
         return await database.fetch_all(query)
 
     async def create(self, post: PostIn) -> int:
         command = posts.insert().values(
             title=post.title,
             content=post.content,
+            published_at=post.published_at,
             published=post.published,
-            published_at=post.published_at
         )
         return await database.execute(command)
-    
+
     async def read(self, id: int) -> Record:
         return await self.__get_by_id(id)
 
-    async def update( self, id: int, post: PostUpdateIn) -> Record:
+    async def update(self, id: int, post: PostUpdateIn) -> Record:
         total = await self.count(id)
         if not total:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
-        
+            raise NotFoundPostError
+
         data = post.model_dump(exclude_unset=True)
         command = posts.update().where(posts.c.id == id).values(**data)
         await database.execute(command)
+
         return await self.__get_by_id(id)
-    
+
     async def delete(self, id: int) -> None:
         command = posts.delete().where(posts.c.id == id)
         await database.execute(command)
@@ -39,10 +42,10 @@ class PostService:
         query = "select count(id) as total from posts where id = :id"
         result = await database.fetch_one(query, {"id": id})
         return result.total
-    
-    async def __get_by_id(self, id) -> Record:
+
+    async def __get_by_id(self, id: int) -> Record:
         query = posts.select().where(posts.c.id == id)
         post = await database.fetch_one(query)
         if not post:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+            raise NotFoundPostError
         return post
